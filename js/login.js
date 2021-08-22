f12c8e39caffa4ae6c1cc6339c00e1bc3852fbaa
maiamendi <maiamendizabal@gmail.com>
2021-08-22 18:46:44 -0300

    Arreglo login

--- js/login.js
+++ js/login.js
@@ -10,33 +10,13 @@ document.addEventListener("DOMContentLoaded", function() {
     evento.preventDefault();
     var usuario = document.getElementById('usuario').value;
     if(usuario.length == 0) {
-      alert('El campo no puede estar vacío');
+      alert('El campo usuario no puede estar vacío');
       return;
     }
     var password = document.getElementById('password').value;
-    if (password.length < 0) {
-      alert('El campo no puede estar vacío');
+    if (password.length == 0) {
+      alert('El campo contraseña no puede estar vacío');
       return;
     } window.location.href = "./home.html";
 
   }
-
-
-
-/*var usuario = document.getElementById('usuario').value;
-var password = document.getElementById('password').value;
-
-function validar(){
-if ((usuario !=="") || (password !=="")) {
-    window.location.href = "./index.html";
-} else {
-    alert("los campos no pueden estar vacíos");
-}};
-
-
-
-
-
-document.addEventListener("DOMContentLoaded", function(e){
-
-});*/
