11488d5497bf0617ecd111a77d85266e7f922070
maiamendi <88292797+maiamendi@users.noreply.github.com>
2021-11-14 20:31:35 -0300

    Entrega 7

--- js/cart.js
+++ js/cart.js
@@ -1,44 +1,25 @@
 const CART_INFO_DESAF = "https://japdevdep.github.io/ecommerce-api/cart/654.json"
 
 let productosCarrito = [];
- 
-
-//muestra el subtotal del producto al modificar la cantidad del mismo - función que uso en el onChange
-function updateProductoSubtotal(cantidad, costo, id){
-    if (cantidad > 0){
-   document.getElementById(id).innerHTML = parseFloat(cantidad*costo);
-   updateTotal();
-    }
-    else{
-      //creo una alerta para cuando la cantidad es menor a 1
-      alert("La cantidad debe ser mayor o igual a 1")
-    };
-};
-
-//muestra el total sumando los subtotales 
-function updateTotal(){
-  let total = 0;
-  let subtotales = document.getElementsByClassName("subtotal");
-  for (let sub of subtotales) {
-      total += parseFloat(sub.innerHTML);
-  }
-  document.getElementById("totalFinal").innerHTML = total;
-};
+let total_final = "Total: USD "
+let DOLLAR_SYMBOL = "USD "
+let PERCENTAGE_SYMBOL = '%';
+let porcentajeEnvio = 0.15;
 
 //muestra los productos y los subtotales segun el valor del input
-function showCarrito(){
+function showCarrito() {
   //inicializo el id en 0
-    let index = 0;
-     let htmlToAppend = "";
-    for(let article of productosCarrito){
-       //igualo el id al numero de index en el json, así cada producto tiene su propio id
-        id = index;
-        //paso todo a USD para unificar monedas
-        if (article.currency == "UYU"){
-        article.unitCost = parseFloat(article.unitCost/40);
-        };
-        let subtotal = parseFloat(article.count * article.unitCost);
-        htmlToAppend += `
+  let index = 0;
+  let htmlToAppend = "";
+  for (let article of productosCarrito) {
+    //igualo el id al numero de index en el json, así cada producto tiene su propio id
+    id = index;
+    //paso todo a USD para unificar monedas
+    if (article.currency == "UYU") {
+      article.unitCost = parseFloat(article.unitCost / 40);
+    };
+    let subtotal = parseFloat(article.count * article.unitCost);
+    htmlToAppend += `
         <tr>
           <td>
             <div class="row">
@@ -67,26 +48,79 @@ function showCarrito(){
             </div>
           </td>
         </tr>
-    ` 
+    `
     index++;
-}
-   
-    document.getElementById("shoppingCart").innerHTML += htmlToAppend;
+  }
+
+  document.getElementById("shoppingCart").innerHTML += htmlToAppend;
+};
 
 
-}
+//muestra el subtotal del producto al modificar la cantidad del mismo - función que uso en el onChange
+function updateProductoSubtotal(cantidad, costo, id) {
+  if (cantidad > 0) {
+    document.getElementById(id).innerHTML = parseFloat(cantidad * costo);
+    updateTotalCosts();
+  } else {
+    //creo una alerta para cuando la cantidad es menor a 1
+    alert("La cantidad debe ser mayor o igual a 1")
+  };
+};
+
+
+
+//muestro subtotal, porcentaje de envío y costo total
+function updateTotalCosts() {
+  let subtotalFinal = 0;
+  let subtotales = document.getElementsByClassName("subtotal");
+  for (let sub of subtotales) {
+    subtotalFinal += parseFloat(sub.innerHTML);
+  }
+  document.getElementById("subTotalFinal").innerHTML = DOLLAR_SYMBOL + subtotalFinal;
+
+  let porcentajeEnvioHTML = document.getElementById("porcentajeEnvio");
+  let totalCostHTML = document.getElementById("totalCost");
+
+  let porcentajeEnvioToShow = DOLLAR_SYMBOL + Math.round((porcentajeEnvio * subtotalFinal));
+  let totalCostToShow = DOLLAR_SYMBOL + (Math.round((subtotalFinal * porcentajeEnvio * 100) / 100) + subtotalFinal);
+
+  porcentajeEnvioHTML.innerHTML = porcentajeEnvioToShow;
+  totalCostHTML.innerHTML = totalCostToShow;
+};
+
+
+
+
+
+
 
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
   getJSONData(CART_INFO_DESAF).then(function (resultObj) {
     if (resultObj.status === "ok") {
-       productosCarrito = resultObj.data.articles;
-      
-        showCarrito();
-         updateTotal();
-    }});
-});
+      productosCarrito = resultObj.data.articles;
 
+      showCarrito();
+      updateTotalCosts();
+    }
+  });
+
+//hace la cuenta del costo de envío y el total según el porcentaje del tipo de envío seleccionado
+  document.getElementById("envioPremium").addEventListener("change", function () {
+    porcentajeEnvio = 0.15;
+    updateTotalCosts();
+  });
+
+  document.getElementById("envioExpress").addEventListener("change", function () {
+    porcentajeEnvio = 0.07;
+    updateTotalCosts();
+  });
+
+  document.getElementById("envioStandard").addEventListener("change", function () {
+    porcentajeEnvio = 0.05;
+    updateTotalCosts();
+  });
+});
